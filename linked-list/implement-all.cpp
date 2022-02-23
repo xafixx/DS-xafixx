92fe97024f4c232f0b049eb1f5d27b09441edaf5
Fikri Aqsha <m.fikriaqsha@gmail.com>
2022-02-23 09:12:45 +0700

    Modified print list options, calling deleteAll after program finished, write insertEnd function

--- linked-list/implement-all.cpp
+++ linked-list/implement-all.cpp
@@ -6,24 +6,63 @@ struct node{
 };
 
 void insertFront(node** headFront, int dataFront){
+	node* temp_ifr = (node*)malloc(sizeof(node*));
+	temp_ifr->data = dataFront;
+	temp_ifr->next = *headFront;
+	*headFront = temp_ifr;
 }
 
 void insertEnd(node** headEnd, int dataEnd){
+	node* tempInsertEnd_f = (node*)malloc(sizeof(node*));
+	tempInsertEnd_f->data = dataEnd;
+	tempInsertEnd_f->next = NULL;
+	
+	if(*headEnd == NULL) {
+		*headEnd = tempInsertEnd_f;
+		return;
+	}
+	
+	node* tempInsertEnd_s = *headEnd;
+	while(tempInsertEnd_s->next != NULL) {
+		tempInsertEnd_s = tempInsertEnd_s->next;
+	}
+	tempInsertEnd_s->next = tempInsertEnd_f;
 }
 
 void insertatPos(node** headatPos, int dataatPos, int insertPos){
+	
 }
 
 void deleteNodeAt(node** headDelete, int deletePos){
 }
 
 void deleteAll(node** headDeleteAll){
+	node* temp1 = *headDeleteAll;
+	node* temp2;
+	while(temp1 != NULL) {
+		temp2 = temp1->next;
+		free(temp1);
+		temp1 = temp2;
+	}
+	*headDeleteAll = temp1;
 }
 
 void printList(node* headPrintList) {
+	node* temp_prl = headPrintList;
+	while(temp_prl != NULL) {
+		std::cout << temp_prl->data << "  ";
+		temp_prl = temp_prl->next;
+	}
+	std::cout << "\n";
 }
 
-void reversePrintList(node* headReversePrintList){
+void recursiveReversePrint(node* headrrPrint){
+	// exit condition
+	if(headrrPrint == NULL) {
+		return;
+	}
+	recursiveReversePrint(headrrPrint->next);
+	std::cout << headrrPrint->data << "  ";
 }
 
 void reverseContentOfList(node** headReverseContentOfList){
@@ -32,14 +71,15 @@ void reverseContentOfList(node** headReverseContentOfList){
 int main() {
 	node* head = NULL;
 	int n, data, opt, pos;
+	
 	while(1){
-		std::cout << "================== SELAMAT DATANG LINKED LIST =======================" << "\n";
+		std::cout << "================== WELCOME TO LINKED LIST PROGRAM =======================" << "\n";
 		std::cout << "1. Insert List" << "\n";
 		std::cout << "2. Delete List" << "\n";
 		std::cout << "3. Print List" << "\n";
-		std::cout << "4. Reverse Print" << "\n";
-		std::cout << "5. Reverse List" << "\n";
-		std::cout << "6. Keluar" << "\n";
+		std::cout << "4. Reverse List" << "\n";
+		std::cout << "5. Keluar" << "\n";
+		std::cout << "Enter your choice[1/2/3/4/5]: ";
 		std::cin >> n;
 		
 		if(n == 1){
@@ -66,7 +106,7 @@ int main() {
 		}else if(n == 2){
 			std::cout << "\n1. Delete all content in list" << "\n";
 			std::cout << "2. Delete content at particular position" << "\n";
-			std::cout << "Enter the number choices[1/2]:  ";
+			std::cout << "Enter your choice[1/2]:  ";
 			std::cin >> opt;
 			
 			if(opt == 1){
@@ -80,17 +120,27 @@ int main() {
 			}
 		}else if(n == 3){
 			std::cout << "============== Contents of List =============="<< "\n";
-			printList(head);
-		}else if(n == 4){
-			reversePrintList(head);
-		}else if(n == 5) {
+			std::cout << "1. Print List as it is" << "\n";
+			std::cout << "2. Reversed Print List" << "\n";
+			std::cout << "Enter your choice[1/2] : ";
+			std::cin >> opt;
+			if(opt == 1){
+				printList(head);
+			} else if(opt == 2) {
+				recursiveReversePrint(head);
+			} else{
+				std::cout << "No " << opt << " choices exist" << "\n";
+			}
+		}else if(n == 4) {
 			reverseContentOfList(&head);
-		}else if(n == 6){
-			std::cout << "============== Program ended ==============" << "\n";
-			std::cout << "============== THANK YOU!!! ==============" << "\n";
+		}else if(n == 5){
+			std::cout << "============== Program ended ============== THANK YOU!!! =============="<< "\n";
 			break;
 		} else{
 			std::cout << "Invalid choices " << n << " please enter the valid choice numbers!" << "\n";
 		}
 	}
+	
+	// free all the allocated nodes after program ended
+	deleteAll(&head);
 }
